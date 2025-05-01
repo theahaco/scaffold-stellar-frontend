a1b33083365bd6f977950cc9687dab5f005cfb3c
Pam Selle <pamela.selle@gmail.com>
2025-05-01 12:48:46 -0400

    Install Stellar Design System

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,28 @@
-import Card from "./components/Card.tsx";
-import githubSrc from "./images/github.svg";
-import logoSrc from "./images/logoWDropShadow.svg";
-import styles from "./App.module.css";
+import { Layout, ThemeSwitch } from "@stellar/design-system";
+import { Button, Input } from "@stellar/design-system";
 
 function App() {
   return (
-    <main>
-      <div className={styles.banner}>
-        <nav className={styles.nav}>
-          <a href="https://github.com/loambuild">
-            <img src={githubSrc} alt="GitHub" />
-          </a>
-        </nav>
+    <>
+    <Layout.Header 
+      disableSetThemeOnLoad
+      projectId="Design System"
+      projectTitle="Design System"
+    />
+    <Layout.Content>
+      Hi
+    </Layout.Content>
+      <ThemeSwitch />
 
-        <div className={styles.container}>
-          <h1 className={styles.h1}>
-            <img src={logoSrc} alt="LOAM" />
-          </h1>
-          <h2 className={styles.h2}>Hello Loam</h2>
-          <div className={styles.intro}>
-            <div className={styles.instructions}>
-              <p>
-                This project includes two Loam contracts: <code>Core</code> and{" "}
-                <code>StatusMessage</code>, located in the{" "}
-                <code>contracts/</code> directory.
-              </p>
 
-              <p>
-                When you ran <code>npm run dev</code>, Loam bound these
-                contracts to TypeScript clients in the{" "}
-                <code>src/contracts/</code> directory.
-              </p>
-            </div>
-            <ul role="list" className={styles.linkCardGrid}>
-              <Card
-                href="https://docs.astro.build/"
-                title="Astro Docs"
-                body="Learn how Astro works and explore the official API docs."
-              />
-              <Card
-                href="https://loam.build/"
-                title="Loam Homepage"
-                body="Learn about Loam."
-              />
-            </ul>
-          </div>
-        </div>
-      </div>
-    </main>
+      <Button
+        size="sm"
+        variant="primary"
+      >
+        Button
+      </Button>
+      <Layout.Footer />
+    </>
   );
 }
 
