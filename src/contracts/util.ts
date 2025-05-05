5e948bb4ca1a36dd44d9f2a37a3edc8a12a6302f
Pam Selle <pamela.selle@gmail.com>
2025-05-05 15:32:16 -0400

    Types to fix lint

--- src/contracts/util.ts
+++ src/contracts/util.ts
@@ -1,5 +1,5 @@
 export const rpcUrl =
-  import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "http://localhost:8000/rpc";
+  import.meta.env.PUBLIC_SOROBAN_RPC_URL as string ?? "http://localhost:8000/rpc";
 export const networkPassphrase =
-  import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ??
+  import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE as string ??
   "Standalone Network ; February 2017";
