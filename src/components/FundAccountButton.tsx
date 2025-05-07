f7db291c2c80a25e5bf9ae3df64e14b7915f257b
Pam Selle <pamela.selle@gmail.com>
2025-05-07 10:52:55 -0400

    Leverage useTransition and be aware of account funding status

--- src/components/FundAccountButton.tsx
+++ src/components/FundAccountButton.tsx
@@ -1,39 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useTransition } from 'react';
 import { useNotification } from '../providers/NotificationProvider';
 import { Button } from '@stellar/design-system';
 
 const FundAccountButton: React.FC = () => {
   const { addNotification } = useNotification();
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const [isFunded, setIsFunded] = useState(false);
   // TODO: replace with account from wallet
   const account = "GDVWY6R4RP37DQPAWBNQKQIZAGDVHUAYMYXKUDSY2O7PJWZNSIZIJNHQ";
 
-  const handleFundAccount = async (account: string) => {
-    setIsDisabled(true);
+  const handleFundAccount = (account: string) => {
+    startTransition(async () => {
 
-    addNotification('Funding account, please wait…', 'primary');
-    try {
-      const response = await fetch(`/friendbot?addr=${account}`, {
-        method: 'GET',
-      });
+      addNotification('Funding account, please wait…', 'primary');
+      try {
+        const response = await fetch(`/friendbot?addr=${account}`, {
+          method: 'GET',
+        });
 
-      if (response.ok) {
-        addNotification('Account funded successfully!', 'success');
-      } else {
-        const body = await response.json();
-        addNotification(`Error funding account: ${body?.detail || 'Unknown error'}`, 'error');
+        if (response.ok) {
+          addNotification('Account funded successfully!', 'success');
+          setIsFunded(true);
+
+        } else {
+          const body = await response.json();
+          if (body.detail === "account already funded to starting balance") {
+            setIsFunded(true);
+          }
+          addNotification(`Error funding account: ${body.detail || 'Unknown error'}`, 'error');
+        }
+      } catch (error) {
+        console.error('Error funding account:', error);
+        addNotification('Error funding account. Please try again.', 'error');
       }
-    } catch (error) {
-      console.error('Error funding account:', error);
-      addNotification('Error funding account. Please try again.', 'error');
-    } finally {
-      setIsDisabled(false);
-    }
+    });
   };
 
   return (
     <div>
-      <Button disabled={isDisabled} onClick={handleFundAccount.bind(this, account)} variant="primary" size="md">Fund Account</Button>
+      <Button disabled={isPending || isFunded} onClick={handleFundAccount.bind(this, account)} variant="primary" size="md">Fund Account</Button>
     </div>
   );
 };
