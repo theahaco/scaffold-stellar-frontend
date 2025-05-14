3ef4afc60306a965679e65d18be6c65beeb569d8
hugo-heer <hugo.heer@epita.fr>
2025-05-14 18:32:02 +0200

    chore: added comments

--- src/util/wallet.ts
+++ src/util/wallet.ts
@@ -14,13 +14,13 @@ import { networkPassphrase } from '../contracts/util'
 const kit: StellarWalletsKit = new StellarWalletsKit({
   network: networkPassphrase as WalletNetwork,
   modules: [
-    // new AlbedoModule(),
+    // new AlbedoModule(), omitted bc does not support `getNetwork`
     new FreighterModule(),
-    // new RabetModule(),
-    // new xBullModule(),
-    // new HanaModule(),
+    // new RabetModule(), omitted bc it is not detected even if you have it
+    // new xBullModule(), omitted bc does not support `getNetwork`
+    // new HanaModule(), omitted bc does not support `getNetwork`
     // new LobstrModule(), omitted bc does not support `getNetwork`
-    // new HotWalletModule(),
+    // new HotWalletModule(), omitted bc it keeps opening popups
   ],
   selectedWalletId: storage.getItem("walletId") || FREIGHTER_ID, // should be able to keep it unset!!!
 });
