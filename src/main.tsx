1ebab6293fcfcd322d8873149d0d3807614443b1
hugo-heer <hugo.heer@epita.fr>
2025-05-22 20:11:04 +0200

    fix: removed soroban react

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,13 @@ import App from "./App.tsx";
 import "@stellar/design-system/build/styles.min.css";
 import { WalletProvider } from "./providers/WalletProvider.tsx";
 import { NotificationProvider } from "./providers/NotificationProvider.tsx"
-import MySorobanReactProvider from "./components/MySorobanReactProvider.tsx";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <NotificationProvider>
-      <MySorobanReactProvider>
         <WalletProvider>
           <App />
         </WalletProvider>
-      </MySorobanReactProvider>
     </NotificationProvider>
   </StrictMode>
 );
