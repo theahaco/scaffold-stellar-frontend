9c45a53acaece2d6a8c59c1806de1be46af4839d
Pam Selle <pamela.selle@gmail.com>
2025-06-03 12:06:54 -0400

    Add base URL for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
+    base: '/scaffold-stellar-frontend/',
     plugins: [react()],
     define: {
       global: "window",
