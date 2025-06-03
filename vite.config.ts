c282e6b11ce361beee19d38c044861fe19833881
Pam Selle <pamela.selle@gmail.com>
2025-06-03 12:32:59 -0400

    Move friendbot URL fetch outside vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-function getFriendbotHost(mode: string) {
-  // See https://developers.stellar.org/docs/tools/quickstart/faucet for more information
-  // on friendbot and the local faucet
-  const env = loadEnv(mode, process.cwd(), '')
-  switch (env.PUBLIC_STELLAR_NETWORK) {
-    case "local":
-      return "http://localhost:8000/friendbot";
-    case "futurenet":
-      return "https://friendbot-futurenet.stellar.org";
-    case "testnet":
-      return "https://friendbot.stellar.org";
-    case "mainnet":
-      // friendbot is not available on mainnet, this is a fallback that should not need to be called
-      return "https://friendbot.stellar.org";
-    default:
-      throw new Error(`Unknown PUBLIC_STELLAR_NETWORK: ${env.PUBLIC_STELLAR_NETWORK}`);
-  }
-}
-
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
     define: {
@@ -31,9 +12,8 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/friendbot': {
-          target: getFriendbotHost(mode),
+          target: 'http://localhost:8000/friendbot',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/friendbot/, ''),
         },
       }
     }
