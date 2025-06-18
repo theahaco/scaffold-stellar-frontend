9b6055cfe63a3ef6b46c25cf0131e8e6d001029a
Fabricius Zatti <fazzatti@gmail.com>
2025-06-18 17:40:38 -0300

    feat(debug): add JSON display and transaction visualization components

---
+++ src/debug/components/PrettyJsonTransaction.tsx
@@ -0,0 +1,133 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
+import { useEffect } from "react";
+import { toSafeNumberOrThrow } from "lossless-json";
+
+import { PrettyJson } from "./PrettyJson";
+import { signatureHint } from "../util/signatureHint";
+import { xdrUtils } from "../util/xdrUtils";
+import { formatAmount } from "../util/formatAmount";
+import { getNetworkHeaders } from "../util/getNetworkHeaders";
+
+import { useCheckTxSignatures } from "../hooks/useCheckTxSignatures";
+
+import { AnyObject } from "../types/types";
+import { network } from "../../contracts/util";
+
+type PrettyJsonTransactionProps = {
+  xdr: string;
+  json: AnyObject;
+};
+
+export const PrettyJsonTransaction = ({
+  xdr,
+  json,
+}: PrettyJsonTransactionProps) => {
+  const { data, isFetching, isLoading, refetch } = useCheckTxSignatures({
+    xdr,
+    networkPassphrase: network.passphrase,
+    networkUrl: network.horizonUrl,
+    headers: getNetworkHeaders(network, "horizon"),
+  });
+
+  const isTx = Boolean(json?.tx || json?.tx_fee_bump);
+
+  useEffect(() => {
+    // Check transaction signatures
+    if (isTx) {
+      void refetch();
+    }
+  }, [isTx, refetch]);
+
+  const customValueRenderer = (item: any, key: string, parentKey?: string) => {
+    // Signature hint
+    if (parentKey === "signatures" && key === "hint") {
+      return PrettyJson.renderStringValue({ item: signatureHint(item) });
+    }
+
+    // Signature check
+    if (data && parentKey === "signatures" && key === "signature") {
+      const match = data.find((s) => s.sig.equals(Buffer.from(item, "hex")));
+
+      if (match) {
+        return PrettyJson.renderStringValue({
+          item,
+        });
+      }
+
+      return item;
+    }
+
+    // Amount
+    const amountKeys = [
+      "amount",
+      "buy_amount",
+      "starting_balance",
+      "send_max",
+      "send_amount",
+      "dest_min",
+      "dest_amount",
+      "limit",
+    ];
+
+    if (amountKeys.includes(key)) {
+      const parsedAmount = xdrUtils.fromAmount(item);
+      let formattedAmount = "";
+
+      try {
+        formattedAmount = formatAmount(toSafeNumberOrThrow(parsedAmount));
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (e) {
+        // It might fail for BigInt
+        formattedAmount = formatAmount(parsedAmount as any);
+      }
+
+      if (formattedAmount) {
+        return PrettyJson.renderStringValue({
+          item: `${formattedAmount} (raw: ${item})`,
+          itemType: "number",
+        });
+      }
+
+      return PrettyJson.renderStringValue({ item });
+    }
+
+    // Manage data
+    if (parentKey === "manage_data") {
+      if (key === "data_name") {
+        return PrettyJson.renderStringValue({
+          item: `${item} (hex: ${Buffer.from(item).toString("base64")})`,
+        });
+      }
+
+      if (key === "data_value") {
+        return PrettyJson.renderStringValue({
+          item: `${Buffer.from(item, "hex").toString()} (hex: ${Buffer.from(item, "hex").toString("base64")})`,
+        });
+      }
+    }
+
+    return null;
+  };
+
+  const customKeyRenderer = (item: any, key: string) => {
+    if (key === "signatures" && item?.length > 0) {
+      return <div className="PrettyJson__key__note">· Signatures Checked</div>;
+    }
+
+    return null;
+  };
+
+  return (
+    <PrettyJson
+      json={json}
+      customValueRenderer={customValueRenderer}
+      customKeyRenderer={customKeyRenderer}
+      isLoading={isLoading || isFetching}
+    />
+  );
+};
