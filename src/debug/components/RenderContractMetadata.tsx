9dd7a1f9a3f33f57148a050449f55a5427540846
Fabricius Zatti <fazzatti@gmail.com>
2025-07-08 08:52:18 -0300

    feat(RenderContractMetadata): enhance contract metadata display with improved layout and styling

--- src/debug/components/RenderContractMetadata.tsx
+++ src/debug/components/RenderContractMetadata.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Box } from "../../components/layout/Box";
 import { ContractMetadata } from "../util/loadContractMetada";
-import { Input, Link, Table, Text } from "@stellar/design-system";
-import { AnyObject } from "../types/types";
+import {
+  Badge,
+  Card,
+  Input,
+  Label,
+  Link,
+  Table,
+  Text,
+} from "@stellar/design-system";
 
 interface RenderContractMetadataProps {
   metadata?: ContractMetadata;
@@ -11,7 +18,13 @@ interface RenderContractMetadataProps {
 const metaDocsLink =
   "https://developers.stellar.org/docs/learn/fundamentals/contract-development/overview#contract-meta";
 
+enum ContractMetadataType {
+  SCEnvMetaEntry = "SCEnvMetaEntry",
+  SCMetaEntry = "SCMetaEntry",
+}
+
 interface TableItem {
+  metaType: ContractMetadataType;
   val: string;
   key: string;
   id: string;
@@ -21,17 +34,72 @@ const RenderContractMetadata: React.FC<RenderContractMetadataProps> = ({
 }) => {
   if (!metadata) return null;
 
-  const getTableData = (data: unknown): TableItem[] => {
+  const formatTableItems = (
+    data: unknown,
+    metaType: ContractMetadataType,
+  ): TableItem[] => {
     if (typeof data !== "object" || data === null) {
       return [];
     }
     return Object.entries(data).map(([key, value]) => ({
       key: String(key),
       val: value as string,
-
       id: String(key),
+      metaType: metaType,
     }));
   };
+
+  const getTableData = (): TableItem[] => {
+    const tableEntries: TableItem[] = [
+      ...formatTableItems(
+        metadata.contractmetav0,
+        ContractMetadataType.SCMetaEntry,
+      ),
+      ...formatTableItems(
+        metadata.contractenvmetav0,
+        ContractMetadataType.SCEnvMetaEntry,
+      ),
+    ];
+    return tableEntries;
+  };
+
+  const renderTextCell = (item: string) => (
+    <td
+      style={{
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+        paddingTop: "0.5rem",
+        paddingBottom: "0.5rem",
+      }}
+    >
+      <Text as="p" size="xs">
+        {item}
+      </Text>
+    </td>
+  );
+
+  const renderBadgeCell = (item: string) => (
+    <td
+      style={{
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+        paddingTop: "0.5rem",
+        paddingBottom: "0.5rem",
+      }}
+    >
+      <Badge size="sm" variant="secondary">
+        {item}
+      </Badge>
+    </td>
+  );
+
+  const renderRow = (key: string, val: string, type: ContractMetadataType) => (
+    <>
+      {renderBadgeCell(type)}
+      {renderTextCell(key)}
+      {renderTextCell(val)}
+    </>
+  );
 
   return (
     <>
@@ -47,55 +115,41 @@ const RenderContractMetadata: React.FC<RenderContractMetadataProps> = ({
       />
 
       <Box gap="xs" direction="column">
-        <h4 style={{ marginBottom: "0.5rem" }}>Metadata </h4>
-        <Text as="span" size="xs" style={{ marginBottom: "0.5rem" }}>
-          This section contains the metadata of the contract, which is a
-          collection of key-value pairs that provide additional information
-          about the contract. See{" "}
-          <Link href={metaDocsLink} target="_blank" rel="noopener noreferrer">
-            Contract Metadata Documentation
-          </Link>{" "}
-          for further details.
-        </Text>
-        <>
-          <Table
-            breakpoint={300}
-            hideNumberColumn
-            columnLabels={[]}
-            data={getTableData({
-              ...(metadata.contractmetav0 as AnyObject),
-              ...(metadata.contractenvmetav0 as AnyObject),
-            })}
-            renderItemRow={(item: TableItem) => (
-              <>
-                <td
-                  style={{
-                    paddingLeft: "1rem",
-                    paddingRight: "1rem",
-                    paddingTop: "0.5rem",
-                    paddingBottom: "0.5rem",
-                  }}
-                >
-                  <Text as="p" size="xs">
-                    {item.key}
-                  </Text>
-                </td>
-                <td
-                  style={{
-                    paddingLeft: "1rem",
-                    paddingRight: "1rem",
-                    paddingTop: "0.5rem",
-                    paddingBottom: "0.5rem",
-                  }}
-                >
-                  <Text as="p" size="xs">
-                    {item.val}
-                  </Text>
-                </td>
-              </>
-            )}
-          />
-        </>
+        <Label size="sm" htmlFor="contract-metadata">
+          Contract Metadata
+        </Label>
+        <Card variant="primary">
+          <Text as="span" size="xs" style={{ marginBottom: "0.5rem" }}>
+            This section contains the metadata of the contract, which is a
+            collection of key-value pairs that provide additional information
+            about the contract. See{" "}
+            <Link href={metaDocsLink} target="_blank" rel="noopener noreferrer">
+              Contract Metadata Documentation
+            </Link>{" "}
+            for further details.
+          </Text>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Table
+              breakpoint={300}
+              hideNumberColumn
+              columnLabels={[
+                { id: "type", label: "Type" },
+                { id: "key", label: "Key" },
+                { id: "value", label: "Value" },
+              ]}
+              data={getTableData()}
+              renderItemRow={(item: TableItem) =>
+                renderRow(item.key, item.val, item.metaType)
+              }
+            />
+          </div>
+        </Card>
       </Box>
     </>
   );
