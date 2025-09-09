4e75d15813f8d02cc836277b9be4966e136622dd
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2025-09-09 13:43:58 -0400

    feat: add `guesser` arg

--- src/components/GuessTheNumber.tsx
+++ src/components/GuessTheNumber.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Code, Input, Text } from "@stellar/design-system";
 import { useWallet } from "../hooks/useWallet";
 import game from "../contracts/guess_the_number";
+import { wallet } from "../util/wallet";
 
 export const GuessTheNumber = () => {
   const [guessedIt, setGuessedIt] = useState<boolean>();
@@ -18,7 +19,13 @@ export const GuessTheNumber = () => {
 
   const submitGuess = async () => {
     if (!theGuess) return;
-    const { result } = await game.guess({ a_number: BigInt(theGuess) });
+    const tx = await game.guess({
+      guesser: address,
+      a_number: BigInt(theGuess),
+    });
+    const { result } = await tx.signAndSend({
+      signTransaction: wallet.signTransaction.bind(game),
+    });
     setGuessedIt(result);
   };
 
